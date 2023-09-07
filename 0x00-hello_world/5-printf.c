30b841d65317a0d9458bf999aa55b88c4fe602c2
Jusca <jusca.mol@gmail.com>
2023-09-07 00:17:24 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * descriptio - writing a script
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
