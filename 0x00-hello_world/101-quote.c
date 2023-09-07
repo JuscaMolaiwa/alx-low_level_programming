6744644bd409321088bb58eec31a574dce207f19
Jusca <jusca.mol@gmail.com>
2023-09-07 00:21:29 -0700

    quite

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: printe a quoate using write function
+ * write(int fd.const void*buf.size_t count)
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
