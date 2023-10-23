8267a298dc9226a6b6db50cba27fcc9ef9b7755d
Jusca <jusca.mol@gmail.com>
2023-10-23 09:56:46 -0700

    printlists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,9 +3,9 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
- *
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
     size_t num = 0;
@@ -16,6 +16,5 @@ size_t print_listint(const listint_t *h)
         num++;
         h = h->next;
     }
-
     return (num);
 }
