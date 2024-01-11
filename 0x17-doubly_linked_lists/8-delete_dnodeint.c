0cfcd3c7babced8cb13d2873169aec3622daa51b
JuscaMolaiwa <jusca.mol@gmail.com>
2024-01-11 13:20:37 +0000

    delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,44 +1,45 @@
 #include "lists.h"
-#include <stdlib.h>
-
 /**
- * delete_dnodeint_at_index - Deletes the node at a given index in a dlistint_t list.
- * @head: Pointer to the address of the head of the list.
- * @index: Index of the node to be deleted (starting from 0).
- *
- * Return: 1 if deletion succeeded, -1 if it failed.
+ * delete_dnodeint_at_index - Delete node at nth
+ * @head: Pointer to direction of the head
+ * @index: The position in de ll
+ * Return: 1 if it succeeded or -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current, *temp;
-    unsigned int i;
+	dlistint_t *p1, *res_mem = *head;
+	unsigned int counter = 0;
 
-    if (head == NULL || *head == NULL)
-        return -1;
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current = *head;
+	if (index == 0)
+	{
+		*head = res_mem->next;
+		if (res_mem->next == NULL)
+			return (-1);
+		res_mem->next->prev = NULL;
+		free(res_mem);
+		return (1);
+	}
 
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return 1;
-    }
-
-    for (i = 0; i < index && current != NULL; i++)
-        current = current->next;
-
-    if (current == NULL)
-        return -1;
-
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-
-    free(current);
-    return 1;
+	while (counter < index)
+	{
+		if (res_mem->next == NULL)
+			return (-1);
+		res_mem = res_mem->next;
+		counter++;
+	}
+	res_mem->prev->next = res_mem->next;
+	if (res_mem->next)
+		res_mem->next->prev = res_mem->prev;
+	if (res_mem->next == NULL)
+	{
+		p1 = res_mem->prev;
+		p1->next = NULL;
+		free(res_mem);
+		return (1);
+	}
+	free(res_mem);
+	return (1);
 }
