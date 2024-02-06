6c548cd36969d7aa31b231f20ce05f416a5f7ddf
JuscaMolaiwa <jusca.mol@gmail.com>
2024-02-06 19:59:48 +0000

    hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,26 +6,21 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *bucket, *aux_free;
-    unsigned long int i = 0;
+	unsigned long int i = 0;
+	hash_node_t *node;
 
-    if (!ht)
-        return;
-
-    for (i = 0; i < ht->size; i++)
-    {
-        bucket = ht->array[i];
-        while (bucket)
-        {
-            aux_free = bucket;
-            bucket = bucket->next;
-            if (aux_free->key)
-                free(aux_free->key);
-            if (aux_free->value)
-                free(aux_free->value);
-            free(aux_free);
-        }
-    }
-    free(ht->array);
-    free(ht);
+	while (i < ht->size)
+	{
+		while (ht->array[i] != NULL)
+		{
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = node;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
 }
